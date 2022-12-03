24204e81a8860b834066e97937181eddebf14b78
billydkiddo <113756889+billydkiddo@users.noreply.github.com>
2022-12-03 15:25:52 +0800

    first commit

---
+++ src/pages/Features.js
@@ -0,0 +1,8 @@
+function Features(){
+  return(
+    <div className='container'>
+    <h1 style={{textAlign: "center"}}>features here</h1>
+    </div>
+  )
+}
+ export default Features;
---
+++ src/pages/Prices.js
@@ -0,0 +1,9 @@
+function Prices(){
+  return(
+    <div className='container'>
+    <h1 style={{textAlign: "center"}}>Prices here</h1>
+    
+    </div>
+  )
+}
+ export default Prices;
